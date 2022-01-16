156d31ddc69eda8cb9dbbf815008dba7dba79a79
hesham1221 <89036077+hesham1221@users.noreply.github.com>
2022-01-16 16:59:29 +0200

    Update firebase-confige.js

--- src/firebase-confige.js
+++ src/firebase-confige.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDK0uPkB-i40jL3pjhq-KPAXYTOUFku7EE",
-  authDomain: "whatsapp-clone-b06a1.firebaseapp.com",
-  projectId: "whatsapp-clone-b06a1",
-  storageBucket: "whatsapp-clone-b06a1.appspot.com",
-  messagingSenderId: "629997414579",
-  appId: "1:629997414579:web:cd733645c33dcf525ce8c6"
+  apiKey: "apikey",
+  authDomain: "authDomain",
+  projectId: "projectid",
+  storageBucket: "storageBucket",
+  messagingSenderId: "message",
+  appId: "appid"
 };
 
 // Initialize Firebase
