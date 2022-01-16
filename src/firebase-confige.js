62f2bca69058e16fe93fd98acf888596fcb2b5bc
hesham1221 <89036077+hesham1221@users.noreply.github.com>
2022-01-16 16:57:48 +0200

    Add files via upload
    coding the header part of the sidebar

---
+++ src/firebase-confige.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDK0uPkB-i40jL3pjhq-KPAXYTOUFku7EE",
+  authDomain: "whatsapp-clone-b06a1.firebaseapp.com",
+  projectId: "whatsapp-clone-b06a1",
+  storageBucket: "whatsapp-clone-b06a1.appspot.com",
+  messagingSenderId: "629997414579",
+  appId: "1:629997414579:web:cd733645c33dcf525ce8c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
